0032256379519fb6036a41d7b6d2081049898b1c
SaumyaBhushan <saumya@knoldus.com>
2022-09-29 08:34:55 +0530

    added .tf files for deploying docker images to ECS Cluster

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.21.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.30.0"
+    }
+  }
+}
+ 
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+provider "docker" {}
